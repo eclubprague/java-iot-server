9d3cac66bd3571cd419ebf5dad1db519d01d96ba
tomikpik <tomas.pikous@gmail.com>
2016-09-06 14:50:04 +0200

    minor fixes

--- src/main/java/cz/eclub/iot/model/DAO/SensorEntryDao.java
+++ src/main/java/cz/eclub/iot/model/DAO/SensorEntryDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class SensorEntryDao extends AbstractDao<SensorEntryEntity> {
 
     public Collection<SensorEntryEntity> getByUUIDLimit(String uuid, String unit, int limit) {
-        Query q = getEntityManager().createQuery("select s from SensorEntryEntity as s where s.unit=:unit and s.sensor._UUID=:uuid");
+        Query q = getEntityManager().createQuery("select s from SensorEntryEntity as s where s.unit=:unit and s.sensor._UUID=:uuid order by s.timestamp desc");
         q.setParameter("unit", unit);
         q.setParameter("uuid", uuid);
         if (limit > 0)
@@ -20,7 +20,7 @@ public class SensorEntryDao extends AbstractDao<SensorEntryEntity> {
     }
 
     public Collection<SensorEntryEntity> getByUUIDLimit(String uuid, int limit) {
-        Query q = getEntityManager().createQuery("select s from SensorEntryEntity as s where s.sensor._UUID=:uuid");
+        Query q = getEntityManager().createQuery("select s from SensorEntryEntity as s where s.sensor._UUID=:uuid order by s.timestamp desc");
         q.setParameter("uuid", uuid);
         if (limit > 0)
             q.setMaxResults(limit);
