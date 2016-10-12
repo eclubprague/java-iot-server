7cabc09ff0df090b07eaeabd900aa8d69938cbbb
novotmike <novotasscz@gmail.com>
2016-10-12 14:39:10 +0200

    Another fixes.

--- src/main/java/cz/eclub/iot/model/DAO/SensorEntryDao.java
+++ src/main/java/cz/eclub/iot/model/DAO/SensorEntryDao.java
@@ -11,25 +11,23 @@ import java.util.List;
 public class SensorEntryDao extends AbstractDao<SensorEntryEntity> {
 
     public Collection<SensorEntryEntity> getByUUIDLimit(String uuid, String unit, int limit) {
-        EntityManager entityManager = DbUtils.getInstance().getEntityManager();
-        Query q = entityManager.createQuery("select s from SensorEntryEntity as s where s.unit=:unit and s.sensor._UUID=:uuid order by s.timestamp desc");
+        Query q = getEntityManager().createQuery("select s from SensorEntryEntity as s where s.unit=:unit and s.sensor._UUID=:uuid order by s.timestamp desc");
         q.setParameter("unit", unit);
         q.setParameter("uuid", uuid);
         if (limit > 0)
             q.setMaxResults(limit);
         List result = q.getResultList();
-        entityManager.close();
+        closeEntityManager();
         return result;
     }
 
     public Collection<SensorEntryEntity> getByUUIDLimit(String uuid, int limit) {
-        EntityManager entityManager = DbUtils.getInstance().getEntityManager();
-        Query q = entityManager.createQuery("select s from SensorEntryEntity as s where s.sensor._UUID=:uuid order by s.timestamp desc");
+        Query q = getEntityManager().createQuery("select s from SensorEntryEntity as s where s.sensor._UUID=:uuid order by s.timestamp desc");
         q.setParameter("uuid", uuid);
         if (limit > 0)
             q.setMaxResults(limit);
         List result = q.getResultList();
-        entityManager.close();
+        closeEntityManager();
         return result;
     }
 }
