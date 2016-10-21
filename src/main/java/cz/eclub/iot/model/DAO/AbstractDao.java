997cb6d8bd31977535ea61f22068a3f5529c53e7
tomikpik <tomas.pikous@gmail.com>
2016-10-21 10:07:23 +0200

    transactions

--- src/main/java/cz/eclub/iot/model/DAO/AbstractDao.java
+++ src/main/java/cz/eclub/iot/model/DAO/AbstractDao.java
@@ -44,9 +44,9 @@ public class AbstractDao<T extends IEntity> implements IAbstractDao<T>, Serializ
     public boolean addNew(T entity) { //TODO should throw exception
         boolean returnValue = false;
         try {
-            //DbUtils.getInstance().getTransactionManager().begin();
+            DbUtils.getInstance().getTransactionManager().begin();
             getEntityManager().persist(entity);
-            //DbUtils.getInstance().getTransactionManager().commit();
+            DbUtils.getInstance().getTransactionManager().commit();
             returnValue = true;
         } catch (Exception e) {
             System.out.println("daco sa pojebalo");
@@ -97,9 +97,9 @@ public class AbstractDao<T extends IEntity> implements IAbstractDao<T>, Serializ
     public boolean update(T entity) { //TODO should throw exception
         boolean returnValue = false;
         try {
-            //DbUtils.getInstance().getTransactionManager().begin();
+            DbUtils.getInstance().getTransactionManager().begin();
             getEntityManager().merge(entity);
-            //DbUtils.getInstance().getTransactionManager().commit();
+            DbUtils.getInstance().getTransactionManager().commit();
             returnValue = true;
         } catch (Exception e) {
             rollback(e);
@@ -113,9 +113,9 @@ public class AbstractDao<T extends IEntity> implements IAbstractDao<T>, Serializ
     public boolean delete(T entity) { //TODO should throw exception
         boolean returnValue = false;
         try {
-            //DbUtils.getInstance().getTransactionManager().begin();
+            DbUtils.getInstance().getTransactionManager().begin();
             getEntityManager().remove(entity);
-            //DbUtils.getInstance().getTransactionManager().commit();
+            DbUtils.getInstance().getTransactionManager().commit();
             returnValue = true;
         } catch (Exception e) {
             e.printStackTrace();
