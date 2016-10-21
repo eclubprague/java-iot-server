0648f692ea18a6b8eb2fd5908de1fc355ebc6183
tomikpik <tomas.pikous@gmail.com>
2016-10-21 08:36:07 +0200

    removed transactions

--- src/main/java/cz/eclub/iot/model/DAO/AbstractDao.java
+++ src/main/java/cz/eclub/iot/model/DAO/AbstractDao.java
@@ -44,13 +44,13 @@ public class AbstractDao<T extends IEntity> implements IAbstractDao<T>, Serializ
     public boolean addNew(T entity) { //TODO should throw exception
         boolean returnValue = false;
         try {
-            //DbUtils.getInstance().getTransactionManager().begin();
+            DbUtils.getInstance().getTransactionManager().begin();
             getEntityManager().persist(entity);
-            //DbUtils.getInstance().getTransactionManager().commit();
+            DbUtils.getInstance().getTransactionManager().commit();
             returnValue = true;
         } catch (Exception e) {
             System.out.println("daco sa pojebalo");
-            //e.printStackTrace();
+            e.printStackTrace();
         } finally {
             closeEntityManager();
         }
