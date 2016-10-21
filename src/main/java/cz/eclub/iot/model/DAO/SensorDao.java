f10a1a3c40ef230329aafb9fe4842aada66cd6f2
tomikpik <tomas.pikous@gmail.com>
2016-10-21 15:42:44 +0200

    delete sensor fixed...maybe

--- src/main/java/cz/eclub/iot/model/DAO/SensorDao.java
+++ src/main/java/cz/eclub/iot/model/DAO/SensorDao.java
@@ -2,11 +2,6 @@ package cz.eclub.iot.model.DAO;
 
 import cz.eclub.iot.model.DbUtils;
 import cz.eclub.iot.model.classes.SensorEntity;
-import org.apache.lucene.search.Query;
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.jpa.FullTextQuery;
-import org.hibernate.search.jpa.Search;
-import org.hibernate.search.query.dsl.QueryBuilder;
 
 import java.util.Collection;
 import java.util.List;
@@ -17,27 +12,13 @@ public class SensorDao extends AbstractDao<SensorEntity> {
     public SensorEntity getByUUID(String UUID) {
         SensorEntity returnValue = null;
         try {
-            FullTextEntityManager ftem = Search.getFullTextEntityManager(getEntityManager());
 
             DbUtils.getInstance().getTransactionManager().begin();
-
-            QueryBuilder b = ftem.getSearchFactory()
-                    .buildQueryBuilder()
-                    .forEntity(SensorEntity.class)
-                    .get();
-
-            Query lq = b.keyword().onField("_UUID").matching(UUID).createQuery();
-
-            FullTextQuery ftQuery = ftem.createFullTextQuery(lq, SensorEntity.class);
-
-            List<SensorEntity> resultList = ftQuery.getResultList();
-
+            List<SensorEntity> results = getEntityManager().createNativeQuery("{\"_UUID\":\""+UUID+"\"}", SensorEntity.class).getResultList();
             DbUtils.getInstance().getTransactionManager().commit();
 
-
-            if (resultList.size() == 1) {
-                returnValue = resultList.get(0);
-            }
+            if (results.size() == 1)
+                returnValue = results.get(0);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,7 +32,6 @@ public class SensorDao extends AbstractDao<SensorEntity> {
     public Collection<SensorEntity> getByUUIDLimit(String uuid, Integer limitResults) {
         return null;
     }
-
 
 
 }
